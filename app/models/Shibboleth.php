a263b060dc607744f9f766540b701697787a1cf6
Eric Bollens <eric@eb.io>
2014-12-18 09:39:29 -0800

    Add uid support

--- app/models/Shibboleth.php
+++ app/models/Shibboleth.php
@@ -6,18 +6,24 @@ class Shibboleth
     {
         return static::user()->eduPersonPrincipalName;
     }
-    
+
     /**
      * @return stdClass
      */
     public static function user()
     {
-        return (object)[
-            'eduPersonPrincipalName' => $_SERVER['SHIB_EPPN'],
-            'eduPersonScopedAffiliation' => $_SERVER['SHIB_EDU_PERSON_AFFILIATION'],
-            'sn' => $_SERVER['SHIB_SN'],
-            'givenName' => $_SERVER['SHIB_GIVEN_NAME'],
-            'mail' => $_SERVER['SHIB_MAIL']
+        $data = [
+          'eduPersonPrincipalName' => $_SERVER['SHIB_EPPN'],
+          'eduPersonScopedAffiliation' => $_SERVER['SHIB_EDU_PERSON_AFFILIATION'],
+          'sn' => $_SERVER['SHIB_SN'],
+          'givenName' => $_SERVER['SHIB_GIVEN_NAME'],
+          'mail' => $_SERVER['SHIB_MAIL']
         ];
+
+        if(array_key_exists('SHIB_UID', $_SERVER)){
+            $data['uid'] = $_SERVER['SHIB_UID']
+        }
+
+        return (object)$data;
     }
 }
