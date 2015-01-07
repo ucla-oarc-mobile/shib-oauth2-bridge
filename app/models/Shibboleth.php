c726e56e517f927d10916a24c111d9a7eda00f80
Eric Bollens <eric@eb.io>
2015-01-07 12:28:16 -0800

    Add implied Shibboleth affiliation scoping

--- app/models/Shibboleth.php
+++ app/models/Shibboleth.php
@@ -12,9 +12,22 @@ class Shibboleth
      */
     public static function user()
     {
+        $scopedAffiliation = null;
+
+        if(array_key_exists('SHIB_EDU_PERSON_SCOPED_AFFILIATION', $_SERVER)){
+          $scopedAffiliation = $_SERVER['SHIB_EDU_PERSON_SCOPED_AFFILIATION'];
+        }else{
+          $affiliationSuffix = '@'.$_SERVER['SHIB_IMPLIED_AFFILIATION_SCOPE'];
+          $scopedAffiliationArr = [];
+          foreach(explode(';', $_SERVER['SHIB_EDU_PERSON_AFFILIATION']) as $affiliation){
+              $scopedAffiliationArr[] = $affiliation.$affiliationSuffix;
+          }
+          $scopedAffiliation = implode(';', $scopedAffiliationArr);
+        }
+
         $data = [
           'eduPersonPrincipalName' => $_SERVER['SHIB_EPPN'],
-          'eduPersonScopedAffiliation' => $_SERVER['SHIB_EDU_PERSON_AFFILIATION'],
+          'eduPersonScopedAffiliation' => $scopedAffiliation,
           'sn' => $_SERVER['SHIB_SN'],
           'givenName' => $_SERVER['SHIB_GIVEN_NAME'],
           'mail' => $_SERVER['SHIB_MAIL']
